555f04cdfb6dc2e4f5305d34cb0ff1ee9955cdcf
ewarchul <ewarchul@gmail.com>
2024-01-29 19:37:16 +0100

    Run R script in quiet mode

--- src/run-benchmarks.py
+++ src/run-benchmarks.py
@@ -6,9 +6,13 @@ from pathlib import Path
 
 SOURCE_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
 
-def spawn_benchmark_runner(benchmark_spec: Path): 
-    args = shlex.split(f'R --no-save -e "library(cecb); cecb::run_benchmark(\\"{benchmark_spec}\\")"')
-    subprocess.Popen(args) 
+
+def run_benchmark(benchmark_spec: Path):
+    args = shlex.split(
+        f'R --no-save --slave -e "library(cecb); cecb::run_benchmark(\\"{benchmark_spec}\\")"'
+    )
+    subprocess.Popen(args)
+
 
 def distpach_machine(machine_name: str) -> Path:
     if machine_name == "hal":
@@ -17,17 +21,12 @@ def distpach_machine(machine_name: str) -> Path:
         return SOURCE_DIR / "yml" / "borg"
 
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        prog="cec_runner",
-        description="starts CEC benchmark using yaml specification"
+        prog="cec_runner", description="starts CEC benchmark using yaml specification"
     )
-    parser.add_argument('-m', '--machine', choices=['hal', 'borg'], required=True)
+    parser.add_argument("-m", "--machine", choices=["hal", "borg"], required=True)
     args = parser.parse_args()
 
     for spec in distpach_machine(args.machine).iterdir():
-        spawn_benchmark_runner(spec)
-
-
-
+        run_benchmark(spec)
